9411cf4b5a14d84dbdd3debf0fd7ded9dda6c9b8
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-02 20:02:43 +0100

    task-02 completed

---
+++ 02/app.py
@@ -0,0 +1,9 @@
+number = 10
+float_number = 3.14
+
+
+if float_number > number:
+    print("The float number is greater than the integer number.")
+else:
+    print("The float number is not greater than the integer number.")
+
