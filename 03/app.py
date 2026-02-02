5260b3f20ee8b16b2bc1bbc91ca36d276d549761
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-02 20:34:42 +0100

    task-03 completed

---
+++ 03/app.py
@@ -0,0 +1,22 @@
+int_number = 5
+float_number = 7.5
+text = "Hello, Python!"
+bool_var = True
+empty_var = None
+set_var = {1, 2, 3}
+tuple_var = (4, 5, 6)
+list_var = [7, 8, 9]
+
+dict_var = {    "name": "Paweł",
+    "learn": "Python",
+}
+
+print("Integer:", int_number)
+print("Float:", float_number)
+print("String:", text)
+print("Boolean:", bool_var)
+print("NoneType:", empty_var)
+print("Set:", set_var)
+print("Tuple:", tuple_var)
+print("List:", list_var)
+print("Dictionary:", dict_var)
