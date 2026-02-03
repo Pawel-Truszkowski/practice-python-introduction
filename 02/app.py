b01e271f42622e98f51b22d62702aa3cd6cae4b6
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-03 16:10:33 +0100

    task-02 fixed

--- 02/app.py
+++ 02/app.py
@@ -1,6 +1,17 @@
 number = 10
 float_number = 3.14
 
+# Komentarz jednolinowy
+
+
+"""
+Komentarz wieloliniowy
+
+Komentarz wieloliniowy
+
+Komentarz wieloliniowy
+"""
+
 
 if float_number > number:
     print("The float number is greater than the integer number.")
