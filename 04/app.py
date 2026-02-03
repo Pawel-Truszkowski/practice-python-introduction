63f0bed165eb78f3456c0a2b9a9eb62ac1c48351
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-02-03 15:24:08 +0100

    task-04 completed

---
+++ 04/app.py
@@ -0,0 +1,6 @@
+imie = "Paweł"
+tygodnie = 1
+cel = "zostać programistą Python"
+
+print(f"Mam na imię {imie}, od {tygodnie} tygodni uczę się programowania. Moim celem jest {cel}.")
+print("Mam na imię {}, od {} tygodni uczę się programowania. Moim celem jest {}.".format(imie, tygodnie, cel))
